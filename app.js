c5589f8f62acae1cf6785d74bd0fcad015771735
Wolf <d.wolftripp@gmail.com>
2022-08-09 13:09:12 -0700

    add ability for player to see how many guesses it took to win the game

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ const game = {
       guess = parseInt(prompt(`Enter a Number between ${this.smallestNum} and ${this.biggestNum}.`))
       this.prevGuesses.push(guess)
       if (guess > this.secretNum) {
-        prompt(`${guess} was too high, here are your previous guesses: ${this.prevGuesses}. Press Enter to continue.`)
+        prompt(`${guess} was too high, here are your previous guesses: ${this.prevGuesses}.`, `Press Enter to continue.`)
       } else if (guess < this.secretNum) {
-        prompt(`${guess} was too low, here are your previous guesses: ${this.prevGuesses}. Press Enter to continue.`)
+        prompt(`${guess} was too low, here are your previous guesses: ${this.prevGuesses}.`,`Press Enter to continue.`)
       } else {
-        prompt(`You guessed ${this.secretNum}! That was the secret number! Press Enter to continue.`)
+          this.prevGuesses.length === 1 ? prompt(`You guessed ${this.secretNum}! That was the secret number! it took 1 guess!`) : prompt(`You guessed ${this.secretNum}! That was the secret number! it took ${this.prevGuesses.length} guesses!`, `Press Enter to end game.`)
       }
     }
   }
