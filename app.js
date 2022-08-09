ba36be529c8a1dcada5a90c79aee0980fef60b2e
Wolf <d.wolftripp@gmail.com>
2022-08-09 12:53:32 -0700

    clean code and remove console.logs

--- app.js
+++ app.js
@@ -7,19 +7,14 @@ const game = {
   play: function () {
     this.secretNum = Math.floor(Math.random() *
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    console.log(this.secretNum)
-    console.log(this.prevGuesses)
-
     this.getGuess()
   },
   getGuess: function () {
     let guess;
-
-    
-    while (guess !== this.secretNum){
+    while (guess !== this.secretNum) {
       guess = parseInt(prompt(`Enter a Number between ${this.smallestNum} and ${this.biggestNum}.`))
       this.prevGuesses.push(guess)
-      if (guess > this.secretNum){
+      if (guess > this.secretNum) {
         prompt(`${guess} was too high, here are your previous guesses: ${this.prevGuesses}. Press Enter to continue.`)
       } else if (guess < this.secretNum) {
         prompt(`${guess} was too low, here are your previous guesses: ${this.prevGuesses}. Press Enter to continue.`)
@@ -27,10 +22,6 @@ const game = {
         prompt(`You guessed ${this.secretNum}! That was the secret number! Press Enter to continue.`)
       }
     }
-    
-    console.log(guess)
-
-
   }
 }
 
